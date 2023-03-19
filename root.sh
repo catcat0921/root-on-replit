8ba67c95ff5be4c2cd6a0dc650a19ebe3028991c
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-19 11:26:17 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -4,4 +4,7 @@ nix-env -iA nixpkgs.unzip
 unzip yt.zip && rm yt.zip
 unzip root.zip && rm root.zip
 tar -xvf root.tar.xz && rm root.tar.xz
+rm -rf music.mp3 && rm -rf video.mp4
 ./dist/proot -S . /bin/bash
+echo "输入su可以提权"
+rm -rf README.md
